25f2901c32ac8ae0b62bc49310519b35cf589fca
TroyFernandes <18601850+TroyFernandes@users.noreply.github.com>
2026-02-07 22:33:51 -0500

    Bump version to 2.0.2

--- Plugin.cs
+++ Plugin.cs
@@ -36,7 +36,7 @@ namespace MusicBeePlugin
             about.Type = PluginType.General;
             about.VersionMajor = 2;  // your plugin version
             about.VersionMinor = 0;
-            about.Revision = 0;
+            about.Revision = 2;
             about.MinInterfaceVersion = MinInterfaceVersion;
             about.MinApiRevision = MinApiRevision;
             about.ReceiveNotifications = (ReceiveNotificationFlags.PlayerEvents | ReceiveNotificationFlags.TagEvents);
